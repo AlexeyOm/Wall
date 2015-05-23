89ad02fee93f98f9a13a89eee1f81f21a3580756
AlexeyOm <alexey.omelchenko@gmail.com>
2015-05-23 16:49:27 +0300

    Функция outputToDiv
    Добавлена функция вывода размеров панелей в div

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 
 
-  document.getElementById("validateWidth").onclick = function(evt) {alert(calculateWall());};
+  document.getElementById("validateWidth").onclick = function(evt) {outputToDiv(calculateWall());};
   
   document.getElementById("wallWidth").onchange = function(evt) {validateSize(document.getElementById('wallWidth'));};
   document.getElementById("wallHeight").onchange = function(evt) {validateSize(document.getElementById('wallHeight'));};
@@ -21,32 +21,50 @@ var validateSize = function (checkMe) {
    document.getElementById(checkMe.id + "-help").innerHTML = "<small> Значение не должно быть пустым </small>";
    return false;
   }
- console.log ("значение "+ checkMe.value);
+ //console.log ("значение "+ checkMe.value);
 
-  if (!/^\d+(\.|,)?\d*$/.test(sizeString)) {
+  if (!/^\d+\.?\d*$/.test(sizeString)) {
     document.getElementById(checkMe.id + "-help").innerHTML = "<small> Введите число </small>";
     return false;
   }
   
+  document.getElementById(checkMe.id + "-help").innerHTML = "Ok";
   return true;
   
     
 };
 
-var calculateWall = function () {
+var calculateWall = function () { //фунцкция возвращает массив массивов (диагональ, количество панелей по ширине, ширина, кол-во пан. по высоте, высота)
   var numPanelsWidth = 0;
-  var numPanelsHorizontal = 0; 
- 
+  var numPanelsHeight = 0; 
+  var panelsNeeded = new Array();
+  var i = 0;
+  var diagSpan = document.getElementById("diagonalChecks");
+  var diagChecks = diagSpan.getElementsByTagName("input");
+  var numOfDiagonals = diagChecks.length;
   
   if(validateSize(document.getElementById('wallWidth')) && validateSize(document.getElementById('wallHeight'))) {
-  //пошли все вычисления
-  // для стены 46 дюймов 
-  numPanelsWidth = Math.round(document.getElementById('wallWidth').value/1.024);
-  numPanelsHorizontal = Math.round(document.getElementById('wallHeight').value/0.579); 
+  for (i = 0; i < numOfDiagonals; i++) {
+    if (diagChecks[i].checked === true ) {
+      numPanelsWidth = Math.max(1, Math.round(document.getElementById('wallWidth').value/(diagChecks[i].dataset.width/1000)));
+      numPanelsHeight = Math.max(1, Math.round(document.getElementById('wallHeight').value/(diagChecks[i].dataset.height/1000))); 
+      console.log(diagChecks[i].dataset.height);
+      panelsNeeded.push(new Array(diagChecks[i].dataset.diag, numPanelsWidth, diagChecks[i].dataset.width, numPanelsHeight, diagChecks[i].dataset.height));
+    }
+  }
  }
   
-  
- return new Array(numPanelsWidth,numPanelsHorizontal); 
-  
+ return panelsNeeded; 
 }
 
+var outputToDiv = function (panelsNeeded) {
+  var i = 0;
+  var sizeString = "";
+  
+ document.getElementById("output").innerHTML = "";
+  for(i = 0; i < panelsNeeded.length; i++) {
+    sizeString = (panelsNeeded[i][1]*panelsNeeded[i][2]/1000).toFixed(1) + "x" + (panelsNeeded[i][3]*panelsNeeded[i][4]/1000).toFixed(1) + " метров ";
+    document.getElementById("output").innerHTML += "На панелях " + panelsNeeded[i][0] + " дюймов. В ширину " + panelsNeeded[i][1] + " панелей, в длину "+ panelsNeeded[i][3] +". Размер: " + sizeString + "<br />";
+  
+  }
+}
